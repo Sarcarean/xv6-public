5ce0bf352a697dbd5947bb5cf10ce474dfd118ea
Michael Harris <contact@embeddedcomputers.net>
2022-04-27 21:22:06 -0700

    Update

--- hw2.c
+++ hw2.c
@@ -5,20 +5,18 @@
 #include "pstat.h"
 
 int main(void) {
+    int i;
+    struct pstat p_stat;
+
     printf(1, "HW2 - Scheduler\n");
     //int num_calls = getsyscallinfo();
     //printf(1, "System calls: %d\n", num_calls);
-    int rc
-    int i;
-    struct pstat ps;
-    rc = getpinfo(&ps);
-    if (rc < 0) {
-        printf(1, "Warning: getpinfo() returned - 1\n");
-    }
+
+    getpinfo(&p_stat);
     printf(1, "PID\tTicks (high)\tTicks (Low)\n");
     for (i = 0; i < NPROC; ++i) {
-        if (ps.inuse[i]) {
-            printf(1, "%d\t%d\t%d\n", ps.pid[i], ps.hticks[i], ps.lticks[i]);
+        if (p_stat.inuse[i]) {
+            printf(1, "%d\t%d\t%d\n", p_stat.pid[i], p_stat.hticks[i], p_stat.lticks[i]);
         }
     }
 
