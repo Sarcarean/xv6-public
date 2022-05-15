7c100b1b4f3662e5c61d477174f9db57990453b4
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 16:39:04 -0700

    Update

--- vm.c
+++ vm.c
@@ -306,6 +306,7 @@ copyuvm(pde_t* pgdir, uint sz) {
     uint pa, i, flags;
     char* mem;
 
+<<<<<<< HEAD
     if ((d = setupkvm()) == 0)
         return 0;
     for (i = 0; i < sz; i += PGSIZE) {
@@ -320,6 +321,23 @@ copyuvm(pde_t* pgdir, uint sz) {
         memmove(mem, (char*)P2V(pa), PGSIZE);
         if (mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
             goto bad;
+=======
+  if((d = setupkvm()) == 0)
+    return 0;
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
+>>>>>>> parent of 7109269 (Modified mem address)
     }
 
     for (i = myproc()->st; i < STACKTOP; i += PGSIZE) {
