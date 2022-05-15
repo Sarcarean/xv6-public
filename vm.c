79a7db55e92001006d954dfa8e2017c267bcc3f8
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 16:26:25 -0700

    Added user stack space to start at high

--- vm.c
+++ vm.c
@@ -310,38 +310,83 @@ clearpteu(pde_t *pgdir, char *uva)
   *pte &= ~PTE_U;
 }
 
-// Given a parent process's page table, create a copy
-// of it for a child.
-pde_t*
-copyuvm(pde_t *pgdir, uint sz)
-{
-  pde_t *d;
-  pte_t *pte;
-  uint pa, i, flags;
-  char *mem;
+//// Given a parent process's page table, create a copy
+//// of it for a child.
+//pde_t*
+//copyuvm(pde_t *pgdir, uint sz)
+//{
+//  pde_t *d;
+//  pte_t *pte;
+//  uint pa, i, flags;
+//  char *mem;
+//
+//  if((d = setupkvm()) == 0)
+//    return 0;
+//  for(i = 0; i < sz; i += PGSIZE){
+//    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+//      panic("copyuvm: pte should exist");
+//    if(!(*pte & PTE_P))
+//      panic("copyuvm: page not present");
+//    pa = PTE_ADDR(*pte);
+//    flags = PTE_FLAGS(*pte);
+//    if((mem = kalloc()) == 0)
+//      goto bad;
+//    memmove(mem, (char*)P2V(pa), PGSIZE);
+//    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+//      kfree(mem);
+//      goto bad;
+//    }
+//  }
+//
+//  return d;
+//
+//bad:
+//  freevm(d);
+//  return 0;
+//}
 
-  if((d = setupkvm()) == 0)
-    return 0;
-  for(i = PGSIZE; i < sz; i += PGSIZE){
-    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
-      panic("copyuvm: pte should exist");
-    if(!(*pte & PTE_P))
-      panic("copyuvm: page not present");
-    pa = PTE_ADDR(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto bad;
-    memmove(mem, (char*)P2V(pa), PGSIZE);
-    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
-      kfree(mem);
-      goto bad;
+pde_t*
+copyuvm(pde_t* pgdir, uint sz) {
+    pde_t* d;
+    pte_t* pte;
+    uint pa, i, flags;
+    char* mem;
+
+    if ((d = setupkvm()) == 0)
+        return 0;
+    for (i = 0; i < sz; i += PGSIZE) {
+        if ((pte = walkpgdir(pgdir, (void*)i, 0)) == 0)
+            panic("copyuvm: pte should exist");
+        if (!(*pte & PTE_P))
+            panic("copyuvm: page not present");
+        pa = PTE_ADDR(*pte);
+        flags = PTE_FLAGS(*pte);
+        if ((mem = kalloc()) == 0)
+            goto bad;
+        memmove(mem, (char*)P2V(pa), PGSIZE);
+        if (mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+            goto bad;
     }
-  }
-  return d;
+
+    for (i = myproc()->st; i < STACKTOP; i += PGSIZE) {
+        if ((pte = walkpgdir(pgdir, (void*)i, 0)) == 0)
+            panic("copyuvm: pte should exist");
+        if (!(*pte & PTE_P))
+            panic("copyuvm: page not present");
+        pa = PTE_ADDR(*pte);
+        flags = PTE_FLAGS(*pte);
+        if ((mem = kalloc()) == 0)
+            goto bad;
+        memmove(mem, (char*)P2V(pa), PGSIZE);
+        if (mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+            goto bad;
+    }
+
+    return d;
 
 bad:
-  freevm(d);
-  return 0;
+    freevm(d);
+    return 0;
 }
 
 //PAGEBREAK!
