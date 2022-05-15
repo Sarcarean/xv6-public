85bbc17603cba35e7939310577acc5f4efa7135d
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 16:31:42 -0700

    Small update

--- memlayout.h
+++ memlayout.h
@@ -7,10 +7,10 @@
 // Key addresses for address space layout (see kmap in vm.c for layout)
 #define KERNBASE 0x80000000         // First kernel virtual address
 #define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
-#define STACKTOP (KERNBASE - 4)     // Top of the address of user space
+#define STACKTOP (KERNBASE - 4)
 
 #define V2P(a) (((uint) (a)) - KERNBASE)
-#define P2V(a) ((void *)(((char *) (a)) + KERNBASE))
+#define P2V(a) (((void *) (a)) + KERNBASE)
 
 #define V2P_WO(x) ((x) - KERNBASE)    // same as V2P, but without casts
 #define P2V_WO(x) ((x) + KERNBASE)    // same as P2V, but without casts
