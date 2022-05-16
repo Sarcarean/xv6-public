e786921ec2ba11e65b363af188cbd759013f3f84
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 20:43:27 -0700

    Updated source

--- syscall.c
+++ syscall.c
@@ -15,25 +15,34 @@
 
 // Fetch the int at addr from the current process.
 int
-fetchint(uint addr, int* ip) {
-    if (addr >= STACKTOP)
-        return -1;
-    *ip = *(int*)(addr);
-    return 0;
+fetchint(uint addr, int *ip)
+{
+  struct proc *curproc = myproc();
+
+  if(addr >= curproc->sz || addr+4 > curproc->sz)
+    return -1;
+  *ip = *(int*)(addr);
+  return 0;
 }
 
+// Fetch the nul-terminated string at addr from the current process.
+// Doesn't actually copy the string - just sets *pp to point at it.
+// Returns length of string, not including nul.
 int
-fetchstr(uint addr, char** pp) {
-    char* s, * ep;
-    if (addr >= STACKTOP)
-        return -1;
-    *pp = (char*)addr;
-    ep = (char*)STACKTOP;
-    for (s = *pp; s < ep; s++) {
-        if (*s == 0)
-            return s - *pp;
-    }
+fetchstr(uint addr, char **pp)
+{
+  char *s, *ep;
+  struct proc *curproc = myproc();
+
+  if(addr >= curproc->sz)
     return -1;
+  *pp = (char*)addr;
+  ep = (char*)curproc->sz;
+  for(s = *pp; s < ep; s++){
+    if(*s == 0)
+      return s - *pp;
+  }
+  return -1;
 }
 
 // Fetch the nth 32-bit system call argument.
@@ -47,14 +56,17 @@ argint(int n, int *ip)
 // to a block of memory of size bytes.  Check that the pointer
 // lies within the process address space.
 int
-argptr(int n, char** pp, int size) {
-    int i;
-    if (argint(n, &i) < 0)
-        return -1;
-    if (size < 0 || (uint)i >= STACKTOP || (uint)i + size > STACKTOP)
-        return -1;
-    *pp = (char*)i;
-    return 0;
+argptr(int n, char **pp, int size)
+{
+  int i;
+  struct proc *curproc = myproc();
+ 
+  if(argint(n, &i) < 0)
+    return -1;
+  if(size < 0 || (uint)i >= curproc->sz || (uint)i+size > curproc->sz)
+    return -1;
+  *pp = (char*)i;
+  return 0;
 }
 
 // Fetch the nth word-sized system call argument as a string pointer.
