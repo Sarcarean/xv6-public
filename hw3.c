552dbd1084cd958dcec15e9a2409af07f3063653
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 21:26:54 -0700

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,17 +1,18 @@
 #include "types.h"
+#include "defs.h"
 #include "stat.h"
 #include "user.h"
+#include "proc.h"
 
 #define NULL 0
 
 int main(void) {
     //int* pi;
     //int c;
-    printf(1, "HW3 - Testing null-ptr dereference\n");
     //pi = NULL;
     //c = *pi; // this is a NULL pointer dereference
     //printf(1, "pi value = %x\n", c);
-
+    printf(1, "HW3 - Testing null-ptr dereference\n");
     struct proc* curproc = myproc();
     printf(1, "My stack loocation = %x\n", curproc->st);
 
