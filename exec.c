e1834699f32989b5cddfcb6e076891fe41d34c88
Michael Harris <contact@embeddedcomputers.net>
2022-05-15 18:08:02 -0700

    Update

--- exec.c
+++ exec.c
@@ -61,11 +61,19 @@ exec(char* path, char** argv) {
 
     // Allocate two pages at the next page boundary.
     // Make the first inaccessible.  Use the second as the user stack.
+
+
+
+    //ORG
     sz = PGROUNDUP(sz);
     if ((sz = allocuvm(pgdir, sz, sz + 2 * PGSIZE)) == 0)
         goto bad;
     clearpteu(pgdir, (char*)(sz - 2 * PGSIZE));
     sp = sz;
+
+
+
+
 
     // Push argument strings, prepare rest of stack in ustack.
     for (argc = 0; argv[argc]; argc++) {
@@ -96,6 +104,10 @@ exec(char* path, char** argv) {
     oldpgdir = curproc->pgdir;
     curproc->pgdir = pgdir;
     curproc->sz = sz;
+    // Added
+    curproc->st = (uint)PGROUNDDOWN(sp);
+    curproc->numPages = 1;
+    //
     curproc->tf->eip = elf.entry;  // main
     curproc->tf->esp = sp;
     switchuvm(curproc);
